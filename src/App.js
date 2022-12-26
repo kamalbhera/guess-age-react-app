159bac09f6a70430201c2e82632cffed819b8ac6
Kamal Bhera <kamalbhera96@gmail.com>
2022-12-26 22:54:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const [result, setResult] = useState("");
+
+  var serverURL = "https://api.agify.io/";
+
+  function getTranslationURL(txt) {
+    return serverURL + "?name=" + txt;
+  }
+
+  function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time");
+  }
+
+  function clickEventHandler() {
+    var inputText = text;
+
+    fetch(getTranslationURL(inputText))
+      .then((response) => response.json())
+      .then((json) => {
+        var age = json.age;
+        setResult(age);
+      })
+      .catch(errorHandler);
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +43,12 @@ function App() {
         >
           Learn React
         </a>
+        <textarea
+        placeholder="Enter your name here"
+        onChange={(e) => setText(e.target.value)}
+      ></textarea>
+      <button onClick={() => clickEventHandler()}>Guess my age</button>
+      <div>{result}</div>
       </header>
     </div>
   );
